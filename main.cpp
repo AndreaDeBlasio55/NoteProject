f6a7924a4969c10a05680161f58e4c401c4c5295
AndreaDeBlasio55 <32814522+AndreaDeBlasio55@users.noreply.github.com>
2021-10-19 22:07:08 +0200

    First output works!

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int main() {
 
     Note myFirstNote ("Lunedì", "Spesa", "Cosa comprare alla coop");
+    Note mySecondNote ("Martedì", "Allenamento", "Quali esercizi fare");
 
-    cout << "L'Id della nota" << myFirstNote.getTitle() << " è " << myFirstNote.getId();
+    cout << endl;
+    cout << "L'Id della nota" << myFirstNote.getTitle() << " è " << myFirstNote.getId() << endl;
+    cout << endl;
+    cout << "L'Id della nota" << mySecondNote.getTitle() << " è " << mySecondNote.getId() << endl;
 
     return 0;
 }
