e2ec588253680a899ed3f0eae99f2163e4c680c0
AndreaDeBlasio55 <32814522+AndreaDeBlasio55@users.noreply.github.com>
2021-11-25 18:21:51 +0100

    Cleaning up the code

--- Headers/NoteNew.h
+++ Headers/NoteNew.h
@@ -12,7 +12,7 @@
 #include <iostream>
 #include <string>
 #include "Observer.h"
-#include "Collection.h"
+#include "CollectionNew.h"
 using namespace std;
 
 class NoteNew{
--- test/NoteNewTest.cpp
+++ test/NoteNewTest.cpp
@@ -4,7 +4,6 @@
 
 #include "gtest/gtest.h"
 #include "../Headers/NoteNew.h"
-#include "../Headers/Collection.h"
 
 TEST(NoteNew, Costructor_GOOD){
     NoteNew note("Note 1", "Description note 1", "Collection 1",true, false);
